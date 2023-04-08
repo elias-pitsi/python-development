df64854d56feeb579a3894301df140f66f877a74
Elias <elias@Eliass-MacBook-Air.local>
2023-04-08 22:19:11 +0200

    Added if statements ti the for loop

--- basics/control-flow.py
+++ basics/control-flow.py
@@ -6,5 +6,18 @@
 
 users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
 
+# Strategy: copy the dictionary, then iterate over the copy
 for user, status in users.copy().items():
     print(f"{user} is {status}")
+
+    if status == 'inactive':
+        del users[user]
+
+print("\nnew strategy\n")
+
+# Strategy: Create a new collection of active users
+active_users = {}
+for user, status in users.items():
+    if status == 'active':
+        active_users[user] = status
+        print(f"{user} is {status}")
