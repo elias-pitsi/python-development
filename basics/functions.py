6fdc41d5506b201ef4929211d77a281066185049
elias-pitsi <e19920805@gmail.com>
2023-04-11 09:29:39 +0200

    Added keywords argunents

--- basics/functions.py
+++ basics/functions.py
@@ -43,7 +43,7 @@ the following function accumulates the arguments passed to it on subsequent call
 # print(f2(2))
 # print(f2(3))
 
-# Keywords argumentts 
+# Keywords arguments 
 def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
     print("-- This parrot wouldn't", action, end=' ')
     print("if you put", voltage, "volts through it.", end=' ')
@@ -56,3 +56,23 @@ parrot(voltage=1000000, action='VOOOOOM')             # 2 keyword arguments
 parrot(action='VOOOOOM', voltage=1000000)             # 2 keyword arguments
 parrot('a million', 'bereft of life', 'jump')         # 3 positional arguments
 parrot('a thousand', state='pushing up the daisies')  # 1 positional, 1 keyword
+
+
+# **name: It contains a dictionary of all keyword arguments except for those corresponding to a formal parameter.
+# *name: It contains a tuple of positional arguments beyond the formal parameter list. 
+
+def cheeseshop(kind, *arguments, **keywords):
+    print("-- Do you have any", kind, "?")
+    print("-- I'm sorry, we're all out of", kind)
+    for arg in arguments:
+        print(arg)
+    print("-" * 40)
+    keys = sorted(keywords.keys())
+    for kw in keys:
+        print(kw, ":", keywords[kw])
+
+cheeseshop("Limburger", "It's very runny, sir.",
+           "It's really very, VERY runny, sir.",
+           shopkeeper="Michael Palin",
+           client="John Cleese",
+           sketch="Cheese Shop Sketch")
